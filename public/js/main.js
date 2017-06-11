e783cc35ea24b354884adae14ef0a66bac27d5e4
akshatjain99 <jain.akshat546@gmail.com>
2017-06-11 17:18:07 +0530

    Created Basic Layout

---
+++ public/js/main.js
@@ -0,0 +1,33 @@
+$(function() {
+
+    //Changing the active item in the main menu and sidebar
+    var rel = $('body[rel]').attr('rel');
+    $('.main.menu a:eq(' + rel + ') , #side a:eq(' + rel + ')').addClass('active');
+
+     //fix menu when passed
+      $('.masthead')
+        .visibility({
+          once: false,
+          onBottomPassed: function() {
+            $('.fixed.menu').transition('fade in');
+          },
+          onBottomPassedReverse: function() {
+            $('.fixed.menu').transition('fade out');
+          }
+        })
+      ;
+      //Changing the active item in following menu
+      var rel = $('body[rel]').attr('rel');
+    $('.fixed.menu a:eq(' + rel + ') , #side a:eq(' + rel + ')').addClass('active');
+
+    
+    // create sidebar and attach to menu open
+
+    $('.sidebar-icon > .sidebar.icon').on('click', function() {
+
+        $('.ui.sidebar').sidebar('toggle');
+
+
+    });
+
+});

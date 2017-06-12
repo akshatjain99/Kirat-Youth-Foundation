1b39a56881ec191d3844b184c8c73a275b5a5bf5
akshatjain99 <jain.akshat546@gmail.com>
2017-06-12 20:55:27 +0530

    Fixed header anchor tags

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,10 @@
 $(function() {
 
     //Changing the active item in the main menu and sidebar
-    var rel = $('body[rel]').attr('rel');
-    $('.main.menu a:eq(' + rel + ') , #side a:eq(' + rel + ')').addClass('active');
+    //var rel = $('body[rel]').attr('rel');
+    //$('.main.menu a:eq(' + rel + ') , #side a:eq(' + rel + ')').addClass('active');
+
+
 
     // create sidebar and attach to menu open
 
@@ -19,4 +21,11 @@ $(function() {
   //      });
   //  nav.toggle();
 
+});
+
+
+
+   $("a.menuitem").click(function(){
+   $("a.menuitem.active").removeClass("active");
+   $(this).addClass("active");
 });
